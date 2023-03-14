ffc354ab751c247a771fe8e21d647dfd045058df
Mess <alla-fedia@mail.ru>
2023-03-14 17:26:13 +0300

    4

---
+++ ex_4/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    string n = Convert.ToString(i), f = "Fizz", b = "Buzz", fb = "FizzBuzz";
+    if ((i % 3 == 0) && (i % 5 == 0)) 
+        n = fb;
+    else if (i % 3 == 0)
+        n = f; 
+    else if (i % 5 == 0)
+        n = b;
+    Console.Write($"{n} ");
+}
+
+// FizzBuzz 
